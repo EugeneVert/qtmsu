3cb6ebac92d707c075ec7184256b402abcd9046b
Eugene Vert <eugene.a.vert@gmail.com>
2022-09-12 14:46:39 +0300

    Add kde custom shortcut for dolphin

---
+++ kde-dolphin-shortcuts/README.md
@@ -0,0 +1,4 @@
+# Shortcuts for dolphin using `KDE custom shortcuts`
+
+`<Alt-t>` -- Calls qtmsu on selected file in dolhin.
+This script is using `xdotool` for issuing `<Ctrl-C>` on selected fille and calling qtmsu with clipboard contents (parsed uri) as argument
---
+++ kde-dolphin-shortcuts/qtmsu-dolphin.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os
+from subprocess import run
+from urllib.parse import unquote, urlparse
+
+import pyperclip
+
+
+def main():
+    run(["xdotool", "getactivewindow", "key", "ctrl+c"])
+    c = pyperclip.paste().splitlines()
+    for i in range(len(c)):
+        c[i] = unquote(urlparse(c[i]).path)
+        print(c[i])
+    print(c)
+    os.chdir(os.path.dirname(c[0]))
+    run(["qtmsu", *c])
+
+
+if __name__ == "__main__":
+    main()
